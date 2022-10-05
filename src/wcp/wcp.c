5cb8e6266884c10070ccb698934ea6418ec9b4e3
Milan Toth <milgra@milgra.com>
2022-10-05 11:28:10 +0000

    refactor

--- src/wcp/wcp.c
+++ src/wcp/wcp.c
@@ -1,9 +1,13 @@
 #include "wl_connector.c"
+#include "zc_log.c"
 #include <stdio.h>
+#include <unistd.h>
 
 int main(int argc, char* argv[])
 {
     printf("startup...\n");
+    zc_log_use_colors(isatty(STDERR_FILENO));
+    zc_log_level_debug();
 
     wl_connector_init(300, 200);
 }
--- src/wcp/wl_connector.c
+++ src/wcp/wl_connector.c
@@ -27,6 +27,7 @@ void wl_connector_init(int w, int h);
 #include "xdg-shell-client-protocol.h"
 #include "zc_bm_rgba.c"
 #include "zc_draw.c"
+#include "zc_log.c"
 #include "zc_memory.c"
 
 #define MAX_MONITOR_NAME_LEN 255
@@ -42,40 +43,43 @@ struct monitor_info
     char    name[MAX_MONITOR_NAME_LEN];
 
     enum wl_output_subpixel subpixel;
-
-    struct zxdg_output_v1* xdg_output;
-    struct wl_output*      wl_output;
+    struct zxdg_output_v1*  xdg_output;
+    struct wl_output*       wl_output;
 };
 
 struct wlc_t
 {
-    struct wl_compositor* compositor;
-    struct wl_keyboard*   keyboard;
-    struct wl_surface*    surface;
-    struct wl_display*    display;
-    struct wl_buffer*     buffer;
-    struct wl_seat*       seat;
-    struct wl_shm*        shm;
+    struct wl_display* display; // global display object
 
-    struct zxdg_output_manager_v1* xdg_output_manager;
-    struct zwlr_layer_shell_v1*    layer_shell;
-    struct zwlr_layer_surface_v1*  layer_surface;
+    struct wl_compositor* compositor; // active compositor
+    struct wl_keyboard*   keyboard;   // active keyboard for seat
+    struct wl_seat*       seat;       // active seat
 
-    void* shm_data;
+    struct wl_surface* surface;  // surface for window
+    struct wl_buffer*  buffer;   // buffer for surface
+    struct wl_shm*     shm;      // active shared memory buffer
+    void*              shm_data; // active bufferdata
+
+    struct zxdg_output_manager_v1* xdg_output_manager; // active xdg output manager
+    struct zwlr_layer_shell_v1*    layer_shell;        // active layer shell
+    struct zwlr_layer_surface_v1*  layer_surface;      // active layer surface
+
+    // outputs
 
     int                  monitor_count;
     struct monitor_info* monitors[16];
     struct monitor_info* monitor;
 
-    int win_width;
-    int win_height;
+    // window state
 
+    int  win_width;
+    int  win_height;
     bool running;
 } wlc = {0};
 
 /* ***WL OUTPUT EVENTS*** */
 
-static void wl_connector_wl_output_geometry(
+static void wl_connector_wl_output_handle_geometry(
     void*             data,
     struct wl_output* wl_output,
     int32_t           x,
@@ -89,12 +93,22 @@ static void wl_connector_wl_output_geometry(
 {
     struct monitor_info* monitor = data;
 
-    printf("wl_connector_wl_output_geometry, x %i y %i width_mm %i height_mm %i subpixel %i make %s model %s transform %i for monitor %i\n", x, y, width_mm, height_mm, subpixel, make, model, transform, monitor->index);
+    zc_log_debug(
+	"wl output handle geometry x %i y %i width_mm %i height_mm %i subpixel %i make %s model %s transform %i for monitor %i",
+	x,
+	y,
+	width_mm,
+	height_mm,
+	subpixel,
+	make,
+	model,
+	transform,
+	monitor->index);
 
     monitor->subpixel = subpixel;
 }
 
-static void wl_connector_wl_output_mode(
+static void wl_connector_wl_output_handle_mode(
     void*             data,
     struct wl_output* wl_output,
     uint32_t          flags,
@@ -104,33 +118,38 @@ static void wl_connector_wl_output_mode(
 {
     struct monitor_info* monitor = data;
 
-    printf("wl_connector_wl_output_mode flags %u width %i height %i for monitor %i\n", flags, width, height, monitor->index);
+    zc_log_debug(
+	"wl output handle mode flags %u width %i height %i for monitor %i",
+	flags,
+	width,
+	height,
+	monitor->index);
 
     monitor->physical_width  = width;
     monitor->physical_height = height;
 }
 
-static void wl_connector_wl_output_done(void* data, struct wl_output* wl_output)
+static void wl_connector_wl_output_handle_done(void* data, struct wl_output* wl_output)
 {
     struct monitor_info* monitor = data;
 
-    printf("wl_connector_wl_output_done for monitor %i\n", monitor->index);
+    zc_log_debug("wl output handle done for monitor %i", monitor->index);
 }
 
-static void wl_connector_wl_output_scale(void* data, struct wl_output* wl_output, int32_t factor)
+static void wl_connector_wl_output_handle_scale(void* data, struct wl_output* wl_output, int32_t factor)
 {
     struct monitor_info* monitor = data;
 
-    printf("wl_connector_wl_output_scale %i for monitor %i\n", factor, monitor->index);
+    zc_log_debug("wl output handle scale %i for monitor %i", factor, monitor->index);
 
     monitor->scale = factor;
 }
 
 struct wl_output_listener wl_output_listener = {
-    .geometry = wl_connector_wl_output_geometry,
-    .mode     = wl_connector_wl_output_mode,
-    .done     = wl_connector_wl_output_done,
-    .scale    = wl_connector_wl_output_scale,
+    .geometry = wl_connector_wl_output_handle_geometry,
+    .mode     = wl_connector_wl_output_handle_mode,
+    .done     = wl_connector_wl_output_handle_done,
+    .scale    = wl_connector_wl_output_handle_scale,
 };
 
 /* ***XDG OUTPUT EVENTS*** */
@@ -138,13 +157,13 @@ struct wl_output_listener wl_output_listener = {
 static void wl_connector_xdg_output_handle_logical_position(void* data, struct zxdg_output_v1* xdg_output, int32_t x, int32_t y)
 {
     struct monitor_info* monitor = data;
-    printf("wl_connector_xdg_output_handle_logical_position : %i %i for monitor %i\n", x, y, monitor->index);
+    zc_log_debug("xdg output handle logical position, %i %i for monitor %i", x, y, monitor->index);
 }
 
 static void wl_connector_xdg_output_handle_logical_size(void* data, struct zxdg_output_v1* xdg_output, int32_t width, int32_t height)
 {
     struct monitor_info* monitor = data;
-    printf("wl_connector_xdg_output_handle_logical_size : %i %i for monitor %i\n", width, height, monitor->index);
+    zc_log_debug("xdg output handle logical size, %i %i for monitor %i", width, height, monitor->index);
 
     monitor->logical_width  = width;
     monitor->logical_height = height;
@@ -153,7 +172,7 @@ static void wl_connector_xdg_output_handle_logical_size(void* data, struct zxdg_
 static void wl_connector_xdg_output_handle_done(void* data, struct zxdg_output_v1* xdg_output)
 {
     struct monitor_info* monitor = data;
-    printf("wl_connector_xdg_output_handle_done for monitor %i\n", monitor->index);
+    zc_log_debug("xdg output handle done, for monitor %i", monitor->index);
 }
 
 static void wl_connector_xdg_output_handle_name(void* data, struct zxdg_output_v1* xdg_output, const char* name)
@@ -161,13 +180,13 @@ static void wl_connector_xdg_output_handle_name(void* data, struct zxdg_output_v
     struct monitor_info* monitor = data;
     strncpy(monitor->name, name, MAX_MONITOR_NAME_LEN);
 
-    printf("wl_connector_xdg_output_handle_name : %s for monitor %i\n", name, monitor->index);
+    zc_log_debug("xdg output handle name, %s for monitor %i", name, monitor->index);
 }
 
 static void wl_connector_xdg_output_handle_description(void* data, struct zxdg_output_v1* xdg_output, const char* description)
 {
     struct monitor_info* monitor = data;
-    printf("wl_connector_xdg_output_handle_description : %s for monitor %i\n", description, monitor->index);
+    zc_log_debug("xdg output handle description for monitor %i", description, monitor->index);
 }
 
 struct zxdg_output_v1_listener xdg_output_listener = {
@@ -182,15 +201,16 @@ struct zxdg_output_v1_listener xdg_output_listener = {
 
 static void wl_connector_seat_handle_capabilities(void* data, struct wl_seat* wl_seat, enum wl_seat_capability caps)
 {
-    printf("wl_connector_seat_handle_capabilities : %i\n", caps);
+    zc_log_debug("seat handle capabilities %i", caps);
     if (caps & WL_SEAT_CAPABILITY_KEYBOARD)
     {
 	wlc.keyboard = wl_seat_get_keyboard(wl_seat);
     }
 }
+
 static void wl_connector_seat_handle_name(void* data, struct wl_seat* wl_seat, const char* name)
 {
-    printf("wl_connector_seat_handle_name : %s\n", name);
+    zc_log_debug("seat handle name %s", name);
 }
 
 const struct wl_seat_listener seat_listener = {
@@ -207,24 +227,24 @@ static void wl_connector_handle_global(
     const char*         interface,
     uint32_t            version)
 {
-    printf("wl_connector_handle_global, interface : %s, version %u\n", interface, version);
+    zc_log_debug("handle global, interface : %s, version %u", interface, version);
 
     if (strcmp(interface, wl_compositor_interface.name) == 0)
     {
 	wlc.compositor = wl_registry_bind(registry, name, &wl_compositor_interface, 4);
-	printf("compositor stored\n");
+	zc_log_debug("compositor stored");
     }
     else if (strcmp(interface, wl_seat_interface.name) == 0)
     {
 	wlc.seat = wl_registry_bind(registry, name, &wl_seat_interface, 4);
-	printf("seat stored\n");
+	zc_log_debug("seat stored");
 	wl_seat_add_listener(wlc.seat, &seat_listener, NULL);
-	printf("seat listener added\n");
+	zc_log_debug("seat listener added");
     }
     else if (strcmp(interface, wl_shm_interface.name) == 0)
     {
 	wlc.shm = wl_registry_bind(registry, name, &wl_shm_interface, 1);
-	printf("shm stored\n");
+	zc_log_debug("shm stored");
     }
     else if (strcmp(interface, wl_output_interface.name) == 0)
     {
@@ -237,42 +257,42 @@ static void wl_connector_handle_global(
 
 	// get wl_output events
 	wl_output_add_listener(monitor->wl_output, &wl_output_listener, monitor);
-	printf("wl_output listener added\n");
+	zc_log_debug("wl_output listener added");
 
 	// set up output if it comes after xdg_output_manager_init
 	if (wlc.xdg_output_manager != NULL)
 	{
 	    monitor->xdg_output = zxdg_output_manager_v1_get_xdg_output(wlc.xdg_output_manager, monitor->wl_output);
 	    zxdg_output_v1_add_listener(monitor->xdg_output, &xdg_output_listener, monitor);
-	    printf("xdg_output listener added\n");
+	    zc_log_debug("xdg_output listener added");
 	}
 
 	wlc.monitors[wlc.monitor_count++] = monitor;
-	printf("output stored at index : %u\n", wlc.monitor_count - 1);
+	zc_log_debug("output stored at index : %u", wlc.monitor_count - 1);
     }
     else if (strcmp(interface, zwlr_layer_shell_v1_interface.name) == 0)
     {
 	wlc.layer_shell = wl_registry_bind(registry, name, &zwlr_layer_shell_v1_interface, 1);
-	printf("layer shell stored\n");
+	zc_log_debug("layer shell stored");
     }
     else if (strcmp(interface, zxdg_output_manager_v1_interface.name) == 0)
     {
 	wlc.xdg_output_manager = wl_registry_bind(registry, name, &zxdg_output_manager_v1_interface, 2);
-	printf("xdg output manager stored\n");
+	zc_log_debug("xdg output manager stored");
 
 	// set up outputs if event comes after interface setup
 	for (int index = 0; index < wlc.monitor_count; index++)
 	{
 	    wlc.monitors[index]->xdg_output = zxdg_output_manager_v1_get_xdg_output(wlc.xdg_output_manager, wlc.monitors[index]->wl_output);
 	    zxdg_output_v1_add_listener(wlc.monitors[index]->xdg_output, &xdg_output_listener, wlc.monitors[index]);
-	    printf("xdg_output listener added\n");
+	    zc_log_debug("xdg_output listener added");
 	}
     }
 }
 
 static void wl_connector_handle_global_remove(void* data, struct wl_registry* registry, uint32_t name)
 {
-    printf("wl_connector_handle_global_remove\n");
+    zc_log_debug("handle global remove");
 }
 
 static const struct wl_registry_listener registry_listener =
@@ -302,13 +322,13 @@ int wl_connector_shm_create()
 
     if (shmid < 0)
     {
-	printf("shm_open() failed: %s\n", strerror(errno));
+	zc_log_debug("shm_open() failed: %s", strerror(errno));
 	return -1;
     }
 
     if (shm_unlink(shm_name) != 0)
     {
-	printf("shm_unlink() failed: %s\n", strerror(errno));
+	zc_log_debug("shm_unlink() failed: %s", strerror(errno));
 	return -1;
     }
 
@@ -319,14 +339,14 @@ void* wl_connector_shm_alloc(const int shmid, const size_t size)
 {
     if (ftruncate(shmid, size) != 0)
     {
-	printf("ftruncate() failed: %s\n", strerror(errno));
+	zc_log_debug("ftruncate() failed: %s", strerror(errno));
 	return NULL;
     }
 
     void* buffer = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, shmid, 0);
     if (buffer == MAP_FAILED)
     {
-	printf("mmap() failed: %s\n", strerror(errno));
+	zc_log_debug("mmap() failed: %s", strerror(errno));
 	return NULL;
     }
 
@@ -335,7 +355,7 @@ void* wl_connector_shm_alloc(const int shmid, const size_t size)
 
 static void wl_connector_buffer_release(void* data, struct wl_buffer* wl_buffer)
 {
-    printf("wl_connector_buffer_release\n");
+    zc_log_debug("buffer release");
 }
 
 static const struct wl_buffer_listener buffer_listener = {
@@ -343,7 +363,7 @@ static const struct wl_buffer_listener buffer_listener = {
 
 struct wl_buffer* wl_connector_create_buffer()
 {
-    printf("wl_connector_create_buffer\n");
+    zc_log_debug("create buffer");
 
     double factor = wlc.monitor->scale / ((double) wlc.monitor->physical_width / wlc.monitor->logical_width);
 
@@ -353,26 +373,26 @@ struct wl_buffer* wl_connector_create_buffer()
     int stride = width * 4;
     int size   = stride * height;
 
-    printf("factor : %f, buffer width %i height %i size %i\n", factor, width, height, size);
+    zc_log_debug("factor : %f, buffer width %i height %i size %i", factor, width, height, size);
 
     int fd = wl_connector_shm_create();
     if (fd < 0)
     {
-	fprintf(stderr, "creating a buffer file for %d B failed: %m\n", size);
+	zc_log_error("creating a buffer file for %d B failed: %m", size);
 	return NULL;
     }
-    printf("shm file created\n");
+    zc_log_debug("shm file created");
 
     wlc.shm_data = wl_connector_shm_alloc(fd, size);
 
     if (wlc.shm_data == MAP_FAILED)
     {
-	fprintf(stderr, "mmap failed: %m\n");
+	zc_log_error("mmap failed: %m");
 	close(fd);
 	return NULL;
     }
 
-    printf("shm data created\n");
+    zc_log_debug("shm data created");
 
     struct wl_shm_pool* pool   = wl_shm_create_pool(wlc.shm, fd, size);
     struct wl_buffer*   buffer = wl_shm_pool_create_buffer(pool, 0, width, height, stride, WL_SHM_FORMAT_ARGB8888);
@@ -380,14 +400,14 @@ struct wl_buffer* wl_connector_create_buffer()
     wl_shm_pool_destroy(pool);
 
     wl_buffer_add_listener(buffer, &buffer_listener, NULL);
-    printf("buffer listener added\n");
+    zc_log_debug("buffer listener added");
 
     return buffer;
 }
 
 void wl_connector_draw()
 {
-    printf("draw\n");
+    zc_log_debug("draw");
     double factor = wlc.monitor->scale / ((double) wlc.monitor->physical_width / wlc.monitor->logical_width);
 
     int32_t width  = round_to_int(wlc.monitor->physical_width * factor);
@@ -416,14 +436,14 @@ void wl_connector_draw()
 
 static void wl_connector_layer_surface_configure(void* data, struct zwlr_layer_surface_v1* surface, uint32_t serial, uint32_t width, uint32_t height)
 {
-    printf("wl_connector_layer_surface_configure serial %u width %i height %i\n", serial, width, height);
+    zc_log_debug("layer surface configure serial %u width %i height %i", serial, width, height);
 
     zwlr_layer_surface_v1_ack_configure(surface, serial);
 }
 
 static void wl_connector_layer_surface_closed(void* _data, struct zwlr_layer_surface_v1* surface)
 {
-    printf("wl_connector_layer_surface_closed\n");
+    zc_log_debug("layer surface configure");
 }
 
 struct zwlr_layer_surface_v1_listener layer_surface_listener = {
@@ -433,7 +453,7 @@ struct zwlr_layer_surface_v1_listener layer_surface_listener = {
 
 void wl_connector_init(int w, int h)
 {
-    printf("wl_connector_init\n");
+    zc_log_debug("init %i %i", w, h);
 
     wlc.win_width  = w;
     wlc.win_height = h;
@@ -441,12 +461,12 @@ void wl_connector_init(int w, int h)
     wlc.display = wl_display_connect(NULL);
     if (wlc.display)
     {
-	printf("wl_connector display connected\n");
+	zc_log_debug("display connected");
 
 	struct wl_registry* registry = wl_display_get_registry(wlc.display);
 	wl_registry_add_listener(registry, &registry_listener, NULL);
 
-	printf("wl_connector registry added\n");
+	zc_log_debug("registry added");
 
 	// first roundtrip triggers global events
 	wl_display_roundtrip(wlc.display);
@@ -458,17 +478,17 @@ void wl_connector_init(int w, int h)
 	{
 	    wlc.surface = wl_compositor_create_surface(wlc.compositor);
 
-	    printf("surface created\n");
+	    zc_log_debug("surface created");
 
 	    wlc.monitor = wlc.monitors[0];
 
-	    printf("monitor selected\n");
+	    zc_log_debug("monitor selected");
 
 	    wlc.buffer = wl_connector_create_buffer();
 
-	    printf("buffer created and drawn\n");
+	    zc_log_debug("buffer created and drawn");
 
-	    if (!wlc.layer_shell) printf("Compositor does not implement wlr-layer-shell protocol.\n");
+	    if (!wlc.layer_shell) zc_log_debug("Compositor does not implement wlr-layer-shell protocol.");
 
 	    wlc.layer_surface = zwlr_layer_shell_v1_get_layer_surface(
 		wlc.layer_shell,
@@ -477,7 +497,7 @@ void wl_connector_init(int w, int h)
 		ZWLR_LAYER_SHELL_V1_LAYER_OVERLAY,
 		"wcp");
 
-	    printf("layer surface crated\n");
+	    zc_log_debug("layer surface crated");
 
 	    zwlr_layer_surface_v1_set_size(
 		wlc.layer_surface,
@@ -488,15 +508,15 @@ void wl_connector_init(int w, int h)
 		wlc.layer_surface,
 		ZWLR_LAYER_SURFACE_V1_ANCHOR_RIGHT | ZWLR_LAYER_SURFACE_V1_ANCHOR_TOP);
 
-	    printf("anchor and size set\n");
+	    zc_log_debug("anchor and size set");
 
 	    zwlr_layer_surface_v1_add_listener(wlc.layer_surface, &layer_surface_listener, NULL);
 
-	    printf("layer surface listener added\n");
+	    zc_log_debug("layer surface listener added");
 
 	    wl_surface_set_buffer_scale(wlc.surface, wlc.monitor->scale);
 
-	    printf("scaling set\n");
+	    zc_log_debug("scaling set");
 
 	    wl_surface_commit(wlc.surface);
 
@@ -548,9 +568,9 @@ void wl_connector_init(int w, int h)
 	    /* dmenu_close called */
 	    wl_display_disconnect(wlc.display);
 	}
-	else printf("compositor not received\n");
+	else zc_log_debug("compositor not received");
     }
-    else printf("cannot open display\n");
+    else zc_log_debug("cannot open display");
 }
 
 #endif
